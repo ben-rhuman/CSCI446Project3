5aa8ba08858808ed50d6f817339ba2abfa01a27f
Ben Rhuman <benrhuman@gmail.com>
2016-11-09 16:46:37 -0700

    Add files via upload

---
+++ AIproject3/src/aiproject3/ParserPackage/HouseVotesData.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aiproject3.ParserPackage;
+
+/**
+ *
+ * @author k28h885
+ */
+public class HouseVotesData extends Parser{
+    
+    @Override
+    void convertToNum(){ //Converts the String data into floating point data
+    
+    }
+    
+    @Override
+    void removeID(){} //Does nothing since no IDs to remove
+        
+    @Override
+    void moveClass(){// Moves the classification value to the end of the array
+    
+    }
+}
