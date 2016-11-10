7fc2a4d827a24b7ba3fa47f4ca36b86aa26d64a5
Ben Rhuman <benrhuman@gmail.com>
2016-11-10 13:15:33 -0700

    Update Parser.java

--- AIproject3/src/aiproject3/ParserPackage/Parser.java
+++ AIproject3/src/aiproject3/ParserPackage/Parser.java
@@ -3,33 +3,77 @@
  */
 package aiproject3.ParserPackage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
- * This class reads in and normalizes 
- * the data sets to be generic and compatible 
+ * This class reads in and normalizes the data sets to be generic and compatible
  * with our algorithms.
  */
 public abstract class Parser {
-    
+
+    //Variables
+    ArrayList<ArrayList<String>> group;
+    ArrayList<ArrayList<Double>> data;
+
     //Parser constructor
-    public Parser(){
+    public Parser() {
         readInData();
-        convertToNum();
         removeID();
+        convertToNum();
         replaceMissingValue();
         moveClass();
     }
-    
+
     //Abstract methods
     abstract void convertToNum(); //Converts the data to floating point values
+
     abstract void removeID(); //Removes the IDs in the data sets that require it
+
     abstract void moveClass(); //Moves the data's classification to the end of the array 
-    abstract String fileName(); //Returns the file name
-    
+
+    abstract String fileName();
+
     //Concrete methods
-    private void readInData(){ // Reads in the data from the .txt files
-        
+    public void readInData() { // Reads in the data from the .txt files
+        this.group = new ArrayList<ArrayList<String>>();
+
+        String filePath = new File("").getAbsolutePath() + "\\src\\aiproject3\\ParserPackage\\data\\" + fileName(); //Creates the file path of the desired data set
+        File file = new File(filePath);
+
+        if (file.isFile()) {
+            BufferedReader inputStream = null;
+            try {
+                inputStream = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = inputStream.readLine()) != null) {
+                    ArrayList<String> data = new ArrayList<>();
+                    String[] tokens = line.split(",");
+                    for (String t : tokens) {
+                        data.add(t);
+                    }
+                    group.add(data);
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("file not found");;
+            } catch (IOException ex) {
+            }
+        }
+
+        for (int i = 0; i < group.size(); i++) {
+            for (int j = 0; j < group.get(i).size(); j++) {
+                System.out.print(group.get(i).get(j) + ", ");
+            }
+            System.out.println("");
+        }
     }
-    private void replaceMissingValue(){ //Randomly replaces the missing data with a value in a desired range
-        
+
+    private void replaceMissingValue() { //Randomly replaces the missing data with a value in a desired range
+
     }
 }
+
