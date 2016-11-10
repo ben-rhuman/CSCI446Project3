8aa57592e7324fad01af0218f2383b6b062bd26d
Ben Rhuman <benrhuman@gmail.com>
2016-11-10 13:16:13 -0700

    Update HouseVotesData.java

--- AIproject3/src/aiproject3/ParserPackage/HouseVotesData.java
+++ AIproject3/src/aiproject3/ParserPackage/HouseVotesData.java
@@ -17,11 +17,12 @@ public class HouseVotesData extends Parser{
     }
     
     @Override
-    void removeID(){} //Does nothing since no IDs to remove
-        
-    @Override
-    void moveClass(){// Moves the classification value to the end of the array
-    
+    void moveClass(){// Moves the classification value to the end of the list
+        for(int i = 0; i < data.size(); i++){
+            double temp = data.get(i).get(0);
+            data.get(i).remove(0);
+            data.get(i).add(temp);
+        }
     }
     
     @Override
